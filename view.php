84b10bbbeec057e7978210254b117eb003e6e01e
BBWISE <bayodeblessingakinbola@gmail.com>
2023-08-29 13:17:26 +0100

    Commits from Blessing Bayode, Oro Kwara

--- view.php
+++ view.php
@@ -1,8 +1,9 @@
 <?php
 require_once "backend/Encryption/Encryption.php";
-require_once "./db codes/DBConnector.php";
+require_once "backend/db codes/DBConnector.php";
 
 $connect = DBConnector::connectToDB();
+$enc = new Encryption();
 
 
 $sql = "SELECT * FROM `user`";
@@ -25,19 +26,95 @@ if($result == false){
 else{
 ?>
     <table>
-        
-        <thead>
-            S/N
-        </thead>
-        <thead>
-            S/N
-        </thead>
-        
+        <h1 style="text-align: center">STEAM AI SUMMIT FOR EDUCATORS</h1>
+        <h3 style="text-align: center">Registration form</h3>
+    <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+        S/N
+    </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            Last Name
+        </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            First Name
+        </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            email
+        </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            Phone Number
+        </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            Address
+        </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            Name of School
+        </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            School Address
+        </th>
+        <th style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+            Designation
+        </th>
+
+    <?php
+            $num = 1;
+            foreach($result as $entry){
+    ?>
         <tr>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $num;
+                            $num +=1;
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $enc->getAESDecrypt($entry['lname']);
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $enc->getAESDecrypt($entry['fname']);
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $enc->getAESDecrypt($entry['email']);
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $enc->getAESDecrypt($entry['phone_no']);
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                           echo $enc->getAESDecrypt($entry['address']);
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $enc->getAESDecrypt($entry['schoolName']);
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $enc->getAESDecrypt($entry['schoolAddress']);
+                ?>
+            </td>
+            <td style="border-style:solid; border-width: 1px; font-size: 19px; padding-left: 5px; padding-right: 5px;">
+                <?php
+                            echo $enc->getAESDecrypt($entry['designation']);
+                ?>
+            </td>
 
         </tr>
-        
-    </table>
+    <?php
+            }
+    ?>
+    <tr></tr>
+
+</table>
     <?php
 }
 
